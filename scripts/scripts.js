61352b07dc3b78b9b32f1610351dfc63f1a2c55b
Luis Gaytan <gaytan_luis@msn.com>
2017-06-16 23:27:40 -0700

    first draft

---
+++ scripts/scripts.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+  // Validator
+  $(function() {
+    $('#contact-form').validator();
+    $('#contact-form').on('submit', function(e) {
+      if (!e.isDefaultPrevented()) {
+        var url = "email.php";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $(this).serialize(),
+            success: function(data) {
+                var messageAlert = 'alert-' + data.type;
+                var messageText = data.message;
+
+                var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                if (messageAlert && messageText) {
+                    $('#contact-form').find('.messages').html(alertBox);
+                    $('#contact-form')[0].reset();
+                }
+            }
+        });
+        return false;
+      }
+    })
+  });
+
+
+  // Menu Toggle
+    $(".menu-toggle").on('click', function() {
+      $(this).toggleClass("on");
+      $('.menu-section').toggleClass("on");
+      $(".menu ul").toggleClass('hidden');
+    });
+  // Scroll Features
+  $(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+    $('#main-header').css({
+      'transform': 'translateY(' + wScroll/1.5 + '%)'
+    });
+    if (wScroll > 100){
+      console.log(wScroll);
+      $('.header').addClass('blur-image');
+      $('.logo img').addClass('shrink-logo');
+      $('#main-header').addClass('text-opacity')
+    } else {
+      $('.header').removeClass('blur-image');
+      $('.logo img').removeClass('shrink-logo');
+      $('#main-header').removeClass('text-opacity')
+    }
+
+    if (wScroll > $('.header').offset().top+250){
+      $('.text-center > img').each(function(i){
+        setTimeout(function(){
+          $('.text-center > img').eq(i).css('opacity', '1');
+        }, 250*(i+1));
+      });
+    }
+
+    if (wScroll > $('.services').offset().top+100){
+      $('img').css('top', 0)
+    }
+
+    if (wScroll > $('.secondary').offset().top) {
+      $('#samples > .col-xs-4 > a > img').each(function(i){
+        setTimeout(function(){
+          $('#samples > .col-xs-4 > a > img').eq(i).css('opacity', '1');
+        }, 350*(i+2));
+      });
+    }
+
+    if (wScroll > $('.about').offset().top) {
+      $('#logo-bottom').css('opacity', '1');
+    }
+  });
+})
